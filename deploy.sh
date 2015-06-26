2682ccfa7d52d65350b8cea8b33a1aee3564e0cc
Steve Nay <steve@scnay.com>
2015-06-25 20:17:20 -0600

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,15 +6,27 @@ echo -e "\033[0;32mDeploying updates to Github...\033[0m"
 hugo -s ./
 
 # Add changes to git.
-git add .
+# git add .
 
 # Commit changes.
-msg="Site rebuild `date`"
-if [ $# -eq 1 ]
-    then msg="$1"
-fi
-git commit -m "$msg"
+# msg="Site rebuild `date`"
+# if [ $# -eq 1 ]
+    # then msg="$1"
+# fi
+# git commit -m "$msg"
 
 # Push source and build repos.
-git push origin master
-git subtree push --prefix=public git@github.com:snay2/snay2.github.io.git master
+# git push origin master
+# git subtree push --prefix=public git@github.com:snay2/snay2.github.io.git master
+
+
+# Push to FTP server with mirror mode (requires lftp)
+# Set up ~/.netrc like this:
+# > machine ftp.messagingengine.com
+# >     user yourusername@fastmail.com
+# >     password yourPassword
+lftp -c "set ftp:list-options -a;
+open ftp.messagingengine.com;
+lcd ~/sandbox/globalconstant-blog/public;
+cd /scnay.fastmail.com/files/public_html;
+mirror --reverse --delete --only-newer --use-cache --verbose --allow-chown --allow-suid --no-umask --parallel=2"
